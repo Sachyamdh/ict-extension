8e5bdbe6a7d9e4ae8879b8454980c39b78bbc559
Binit Sapkota <sapkotabinit2002@gmail.com>
2024-11-15 08:58:01 +0545

    changes

--- src/utils/backenFunction.tsx
+++ src/utils/backenFunction.tsx
@@ -1,19 +1,20 @@
 export const sendImageToBackend = async (imageUrl: string) => {
   try {
-
-    const response = await fetch("http://localhost:5000/upload-image", {
+    console.log(imageUrl)
+    const response = await fetch("http://43.245.85.164:8000/upload-image", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ image_url: imageUrl }),
     });
-
+    
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
 
     const result = await response.json();
+    
     return result.is_ai_image; // Assuming this returns true/false
   } catch (error) {
     console.error("Error while fetching from backend:", error);
